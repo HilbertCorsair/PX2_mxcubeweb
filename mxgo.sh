352abd7177aafb0a35f03ae628d18f4892aad0a5
Dan <dt.costin@protonmail.com>
2026-01-22 15:29:43 +0100

    šš§w

--- mxgo.sh
+++ mxgo.sh
@@ -8,5 +8,7 @@ export MURKO_PORT=89011
 export PATH=/nfs/ruche/share-dev/px2dev/MXCuBE/WebApp/mxcubeweb:$PATH
 export PYTHONPATH=/nfs/ruche/share-dev/px2dev/MXCuBE/WebApp/mxcubeweb:$PYTHONPATH
 export PYTHONPATH=/nfs/ruche/share-dev/px2dev/MXCuBE/WebApp/mxcubecore/:$PYTHONPATH
+
+export PYTHONPATH=/nfs/ruche/share-dev/px2dev/MXCuBE/WebApp/experimental_methods/:$PYTHONPATH
 mxcubeweb-server --export-yaml-config $(pwd)/../config/
 mxcubeweb-server -r $(pwd)/../config/ --static-folder $(pwd)/mxcubeweb/ui/build/ -L debug -l $HOME/MXCuBElogs/mxcube.log
